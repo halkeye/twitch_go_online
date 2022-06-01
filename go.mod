aaa7de3b399ef71e1eb3e27786ce0a406ad08815
Gavin Mogan <git@gavinmogan.com>
2022-05-31 22:41:33 -0700

    working

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/halkeye/twitch_go_online
 go 1.17
 
 require (
+	github.com/getsentry/sentry-go v0.13.0
 	github.com/nicklaw5/helix/v2 v2.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
@@ -10,5 +11,5 @@ require (
 
 require (
 	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"text/template"
 	"time"
 
+	sentry "github.com/getsentry/sentry-go"
+	sentryhttp "github.com/getsentry/sentry-go/http"
 	helix "github.com/nicklaw5/helix/v2"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -46,12 +49,12 @@ func fetchStreamInfo(client *helix.Client, user_id string) (*helix.Stream, error
 	return nil, fmt.Errorf("no stream returned for uid: %s", user_id)
 }
 
-func handlerEventSub(secretKey string, client *helix.Client, tmpl *template.Template) http.Handler {
+func handlerEventSub(secretKey string, client *helix.Client, tmpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Read the request body.
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Println(err)
+			panic(errors.Wrap(err, "Error reading incoming post"))
 			return
 		}
 		defer r.Body.Close()
@@ -64,15 +67,14 @@ func handlerEventSub(secretKey string, client *helix.Client, tmpl *template.Temp
 			log.Println("verified signature on message")
 		}
 
-		log.Printf("%s\n", body)
+		log.Printf("Body: %s\n", body)
 
 		// Read the request into eventSubNotification struct.
 
 		var vals eventSubNotification
 		err = json.NewDecoder(bytes.NewReader(body)).Decode(&vals)
 		if err != nil {
-			log.Println(err)
-			return
+			panic(err)
 		}
 
 		// If there's a challenge in the request respond with only the challenge to verify the eventsubscription.
@@ -85,55 +87,139 @@ func handlerEventSub(secretKey string, client *helix.Client, tmpl *template.Temp
 			var onlineEvent helix.EventSubStreamOnlineEvent
 			_ = json.NewDecoder(bytes.NewReader(vals.Event)).Decode(&onlineEvent)
 			log.Printf("got online event for: %s\n", onlineEvent.BroadcasterUserName)
-			w.WriteHeader(200)
-			w.Write([]byte("ok"))
 
 			stream, err := fetchStreamInfo(client, onlineEvent.BroadcasterUserID)
 			if err != nil {
-				log.Error(errors.Wrap(err, fmt.Sprintf("Error fetching stream info for %s (uid: %s)", onlineEvent.BroadcasterUserName, onlineEvent.BroadcasterUserID)))
-				return
+				log.Error(err)
+				panic(errors.Wrap(err, fmt.Sprintf("Error fetching stream info for %s (uid: %s)", onlineEvent.BroadcasterUserName, onlineEvent.BroadcasterUserID)))
 			}
+			w.WriteHeader(200)
+			w.Write([]byte("ok"))
 
 			tmpl.Execute(os.Stderr, map[string]string{
 				"Game":        stream.GameName,
 				"ChannelName": onlineEvent.BroadcasterUserName,
 				"ChannelUrl":  fmt.Sprintf("https://www.twitch.tv/%s", onlineEvent.BroadcasterUserName),
 			})
+
 		} else {
 			log.Errorf("error: event type %s has not been implemented -- pull requests welcome!", r.Header.Get("Twitch-Eventsub-Subscription-Type"))
 		}
 	})
 }
 
-func withLogging(h http.Handler) http.Handler {
-	logFn := func(rw http.ResponseWriter, r *http.Request) {
-		start := time.Now()
+//func withLogging(h http.Handler) http.Handler {
+//  logFn := func(rw http.ResponseWriter, r *http.Request) {
+//    start := time.Now()
 
-		uri := r.RequestURI
-		method := r.Method
-		h.ServeHTTP(rw, r) // serve the original request
+//    uri := r.RequestURI
+//    method := r.Method
+//    h.ServeHTTP(rw, r) // serve the original request
 
-		duration := time.Since(start)
+//    duration := time.Since(start)
 
-		// log request details
-		log.WithFields(log.Fields{
-			"uri":      uri,
-			"method":   method,
-			"duration": duration,
-		})
+//    // log request details
+//    log.WithFields(log.Fields{
+//      "uri":      uri,
+//      "method":   method,
+//      "duration": duration,
+//    })
+//  }
+//  return http.HandlerFunc(logFn)
+//}
+
+func registerSubscription(secretKey string, client *helix.Client, usernames []string, publicUrl string) error {
+	/*
+	* 1) Lookup all usernames and get IDs
+	* 2) Delete all subscriptions with url prefix
+	* 3) Register all userids
+	 */
+
+	getUserResp, err := client.GetUsers(&helix.UsersParams{Logins: usernames})
+	if err != nil {
+		return errors.Wrap(err, "Error getting subscriptions")
 	}
-	return http.HandlerFunc(logFn)
+
+	userIds := []string{}
+
+	for _, userData := range getUserResp.Data.Users {
+		userIds = append(userIds, userData.ID)
+		log.Infof("Monitoring: %s => %s", userData.Login, userData.ID)
+	}
+
+	getSubResp, err := client.GetEventSubSubscriptions(&helix.EventSubSubscriptionsParams{})
+	if err != nil {
+		return errors.Wrap(err, "Error getting subscriptions")
+	}
+
+	for _, sub := range getSubResp.Data.EventSubSubscriptions {
+		if strings.HasPrefix(sub.Transport.Callback, publicUrl) {
+			_, err = client.RemoveEventSubSubscription(sub.ID)
+			if err != nil {
+				return errors.Wrap(err, "Error removing subscriptions")
+			}
+		}
+	}
+
+	createSubResp, err := client.CreateEventSubSubscription(&helix.EventSubSubscription{
+		Type:      helix.EventSubTypeStreamOnline,
+		Version:   "1",
+		Condition: helix.EventSubCondition{BroadcasterUserID: userIds[0]},
+		Transport: helix.EventSubTransport{
+			Method:   "webhook",
+			Callback: fmt.Sprintf("%swebhook/callbacks", publicUrl),
+			Secret:   secretKey,
+		},
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "Error creating subscription")
+	}
+
+	if createSubResp.ErrorStatus > 0 {
+		return errors.Errorf("Error creating subscription (%d) - %s", createSubResp.ErrorStatus, createSubResp.Error)
+	}
+
+	return nil
+}
+
+func mustJson(data interface{}) string {
+	b, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
 }
 
 func main() {
-	tmpl := template.Must(template.New("message").Parse(postMessageTmpl))
-	if tmpl == nil {
-		panic("ahh")
+	// if len(os.Getenv("SENTRY_DSN")) > 0 {
+	err := sentry.Init(sentry.ClientOptions{
+		// Dsn:   os.Getenv("SENTRY_DSN"),
+		Dsn:   "https://9639d9a8f4654a698c71fde09cbb19cd@o76673.ingest.sentry.io/6460422",
+		Debug: true,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
 	}
+	log.Info("Sentry initialized")
+	// Flush buffered events before the program terminates.
+	defer sentry.Flush(2 * time.Second)
+	// }
+	//
+	sentry.CaptureMessage("It works!")
 
 	clientId := os.Getenv("TWITCH_CLIENT_ID")
 	clientSecret := os.Getenv("TWITCH_CLIENT_SECRET")
 	secretKey := os.Getenv("SECRETKEY")
+	publicUrl := os.Getenv("PUBLIC_URL")
+	twitchLogins := os.Getenv("TWITCH_LOGINS")
+	goliveMessage := os.Getenv("GOLIVE_MESSAGE")
+
+	if len(goliveMessage) == 0 {
+		goliveMessage = postMessageTmpl
+	}
+
+	tmpl := template.Must(template.New("message").Parse(goliveMessage))
 
 	if len(secretKey) == 0 {
 		panic("no secret key provided")
@@ -144,12 +230,17 @@ func main() {
 		ClientSecret: clientSecret,
 	})
 	if err != nil {
-		panic(err)
+		err = errors.Wrap(err, "Unable to create twitch client")
+		sentry.CaptureException(err)
+		log.Error(err)
 	}
 
 	resp, err := client.RequestAppAccessToken([]string{"user:read:email"})
 	if err != nil {
-		// handle error
+		err = errors.Wrap(err, "Unable to request app token")
+		sentry.CaptureException(err)
+		log.Error(err)
+		return
 	}
 
 	// Set the access token on the client
@@ -160,8 +251,10 @@ func main() {
 		port = ":" + os.Getenv("PORT")
 	}
 
-	err = registerSubscription(secretKey, client, []string{"halkeye"})
+	err = registerSubscription(secretKey, client, strings.Split(twitchLogins, ","), publicUrl)
 	if err != nil {
+		err = errors.Wrap(err, "Unable to create subscriptions")
+		sentry.CaptureException(err)
 		log.Error(err)
 		return
 	}
@@ -186,75 +279,24 @@ func main() {
 		}()
 	*/
 
-	//webhook/callbacks//webhook/callbacks Listen and serve.
+	// Create an instance of sentryhttp
+	sentryHandler := sentryhttp.New(sentryhttp.Options{
+		Repanic: false,
+	})
+
 	log.Printf("server starting on %s\n", port)
-	http.Handle("/webhook/callbacks", withLogging(handlerEventSub(secretKey, client, tmpl)))
+
+	http.HandleFunc("/webhook/callbacks", sentryHandler.HandleFunc(handlerEventSub(secretKey, client, tmpl)))
+	http.HandleFunc("/foo", sentryHandler.HandleFunc(func(rw http.ResponseWriter, r *http.Request) {
+		panic("y tho")
+	}))
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		io.WriteString(w, "\n")
 	})
-	log.Fatal(http.ListenAndServe(port, nil))
-}
 
-func registerSubscription(secretKey string, client *helix.Client, usernames []string /* url prefix*/) error {
-	/*
-	* 1) Lookup all usernames and get IDs
-	* 2) Delete all subscriptions with url prefix
-	* 3) Register all userids
-	 */
-
-	getUserResp, err := client.GetUsers(&helix.UsersParams{Logins: usernames})
-	if err != nil {
-		return errors.Wrap(err, "Error getting subscriptions")
-	}
-
-	userIds := []string{}
-
-	for _, userData := range getUserResp.Data.Users {
-		userIds = append(userIds, userData.ID)
-	}
-
-	log.Info(mustJson(getUserResp.Data))
-
-	getSubResp, err := client.GetEventSubSubscriptions(&helix.EventSubSubscriptionsParams{})
-	if err != nil {
-		return errors.Wrap(err, "Error getting subscriptions")
-	}
-
-	log.Info(mustJson(getSubResp.Data))
-
-	for _, sub := range getSubResp.Data.EventSubSubscriptions {
-		_, err = client.RemoveEventSubSubscription(sub.ID)
-		if err != nil {
-			return errors.Wrap(err, "Error removing subscriptions")
-		}
-	}
-
-	createSubResp, err := client.CreateEventSubSubscription(&helix.EventSubSubscription{
-		Type:      helix.EventSubTypeStreamOnline,
-		Version:   "1",
-		Condition: helix.EventSubCondition{BroadcasterUserID: userIds[0]},
-		Transport: helix.EventSubTransport{
-			Method:   "webhook",
-			Callback: "https://dev.g4v.dev/webhook/callbacks",
-			Secret:   secretKey,
-		},
-	})
-
-	if err != nil {
-		return errors.Wrap(err, "Error creating subscription")
-	}
-
-	if createSubResp.ErrorStatus > 0 {
-		return errors.Errorf("Error creating subscription (%d) - %s", createSubResp.ErrorStatus, createSubResp.Error)
-	}
-
-	return nil
-}
-
-func mustJson(data interface{}) string {
-	b, err := json.MarshalIndent(data, "", "  ")
-	if err != nil {
+	handler := sentryhttp.New(sentryhttp.Options{}).Handle(http.DefaultServeMux)
+	if err := http.ListenAndServe(port, handler); err != nil {
+		log.Fatal(err)
 		panic(err)
 	}
-	return string(b)
 }
